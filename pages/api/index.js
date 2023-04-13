c0afaa8d77ec4270c40891f2e93b77b86bd8338e
diegoTrueZenit <diego@truezenithcreative.com>
2023-04-13 14:33:10 -0700

    .

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,19 +1,15 @@
-const express = require('express');
-const app = express();
+import NextCors from 'nextjs-cors';
 
-// Allow cross-origin requests from any domain
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+async function handler(req, res) {
+   // Run the cors middleware
+   // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+   await NextCors(req, res, {
+      // Options
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      origin: '*',
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+   });
 
-// Your API routes go here
-app.get('/api', (req, res) => {
-  res.send('Hello, World!');
-});
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+   // Rest of the API logic
+   res.json({ message: 'Hello NextJs Cors!' });
+}
