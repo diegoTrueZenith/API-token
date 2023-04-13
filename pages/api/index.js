d7fb3d0f773553ea853e9739b1b6983dfdf277c9
diegoTrueZenit <diego@truezenithcreative.com>
2023-04-13 14:08:15 -0700

    .

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,11 +1,19 @@
-export default function handler(req, res) {
-    // Handle different HTTP methods
-    if (req.method === 'GET') {
-      res.status(200).json({ message: 'GET request to /api/example' });
-    } else if (req.method === 'POST') {
-      res.status(200).json({ message: 'POST request to /api/example' });
-    } else {
-      res.status(400).json({ message: 'Invalid HTTP method' });
-    }
-  }
-  
+const express = require('express');
+const app = express();
+
+// Allow cross-origin requests from any domain
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Your API routes go here
+app.get('/api', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
