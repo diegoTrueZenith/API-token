2e7e2c8b674d80f7083a6894ef9790b59b8b21ed
diegoTrueZenit <diego@truezenithcreative.com>
2023-04-13 13:33:24 -0700

    allfiles

---
+++ pages/api/token.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default function (req: NextApiRequest, res: NextApiResponse){
+    res.json({num : Math.floor(Math.random() * 100)
+    })
+}
