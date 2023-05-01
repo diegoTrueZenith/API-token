87a81f8d6d5244e4978fc8279f274697c17f727d
diegoTrueZenit <diego@truezenithcreative.com>
2023-05-01 11:30:19 -0700

    .

---
+++ pages/api/guestyReservations.ts
@@ -0,0 +1,20 @@
+import { token } from './token';
+
+export default function handler(req, res) {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        timeout: '50000',
+        authorization: 'Bearer ' + token
+      }
+    };
+    try {
+      fetch('https://open-api.guesty.com/v1/reservations', options)
+      .then(response => response.json())
+      .then(response => res.status(200).json({ response}))
+      // .then(response => console.log(response));
+    } catch (error) {
+      console.error(error);
+    }
+}
