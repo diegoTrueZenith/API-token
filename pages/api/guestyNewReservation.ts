2074915359b91f7672557d866a9d674d32a07f4b
diegoTrueZenit <diego@truezenithcreative.com>
2023-05-11 04:12:48 +0900

    .

--- pages/api/guestyNewReservation.ts
+++ pages/api/guestyNewReservation.ts
@@ -1,5 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "../firebase";
+export const firebaseConfig = {
+  apiKey: "AIzaSyBxItFltQlcaFl5OqDlSyQbX1BuEXieVnY",
+  authDomain: "betterstay-33577.firebaseapp.com",
+  projectId: "betterstay-33577",
+  storageBucket: "betterstay-33577.appspot.com",
+  messagingSenderId: "132048561469",
+  appId: "1:132048561469:web:46b87f20cabbcf61751154"
+};
+
 const app = initializeApp(firebaseConfig);
 import {doc, getDoc, getFirestore, updateDoc } from "firebase/firestore";
 import { useState } from 'react';
