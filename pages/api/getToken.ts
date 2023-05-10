9f257ab7996a354429cc0fca8685324e2e8f007f
diegoTrueZenit <diego@truezenithcreative.com>
2023-05-11 06:19:26 +0900

    .

--- pages/api/getToken.ts
+++ pages/api/getToken.ts
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
 
     async function updateToken(token){
         console.log("=== NEW TOKEN UPDATED  ===");
-        await updateDoc(tokenRef, {token: token.access_token});
+        await updateDoc(tokenRef, {token: token});
         console.log(token);
     }
 }
